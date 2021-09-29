83e096270312219a630b4a5cb9479196cdbedcd1
AzuraOne <meong@meong.com>
2021-09-29 16:18:46 +0700

    Mengeset penjualan

---
+++ coba-coba.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style/dashboard.css">
+</head>
+<body>
+    <div class="all">
+        <div class="header">
+            <div class="pojok"><h1>Dashboard</h1></div>
+            <div class="pojok"><h2>Selamat Datang Nama</h2></div>
+        </div>
+        <div class="bawah" >
+            <ul style="list-style-type: none;">
+                <li>Input Penjualan</li>
+                <li>Stok Barang</li>
+                <li>History</li>
+            </ul>
+        </div>
+        
+        
+
+    </div>
+    
+</body>
+</html>
---
+++ penjualan.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Stok Penjualan</title>
+    <link rel="stylesheet" href="style/dashboard.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="head" style="background-color: blue;">
+        <h1>head</h1>
+    </div>
+    <table border="1">
+        <tr>
+            <th>Nama Produk</th>
+            <th>kode Produk</th>
+            <th>Gambar</th>
+            <th>Status Barang</th>
+        </tr>
+        <tr>
+            <td>Nama Produknya</td>
+            <td>Kode produknya</td>
+            <td>Gambar.png</td>
+            <td>Tersedia</td>
+        </tr>
+        
+    </table>
+    
+</body>
+</html>
