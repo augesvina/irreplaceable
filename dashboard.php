3663b06ecaddebd27d5acbc4722acd74d276b603
AzuraOne <meong@meong.com>
2021-09-29 13:36:40 +0700

    Membuat dashboard

---
+++ dashboard.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dashboard</title>
+    <link rel="stylesheet" href="style/dashboard.css">
+</head>
+<body>
+    <div class="all">
+        <div class="header">
+            <h1>Dashboard</h1>
+            <h1>Selamat Datang Nama</h1>
+        </div>
+        <div class="bawah" >
+            <ul>
+                <li>Input Penjualan</li>
+                <li>Stok Barang</li>
+                <li>History</li>
+            </ul>
+        </div>
+        
+        
+
+    </div>
+    
+</body>
+</html>
